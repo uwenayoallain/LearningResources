a53e35b47145869e86a80d557b94720c5ce95530
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-18 11:43:57 +0200

    learning sql

---
+++ sql/sample.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE sample;
+
+use sample;
+
+CREATE TABLE Users(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(80) NOT NULL,
+    last_name VARCHAR(80) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    password VARCHAR(80) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO Users (first_name, last_name, email, password) VALUES
+    ('John', 'Doe', 'joe@gmail.com', 'password'),
+    ('Jane', 'Doe', 'jane@gmail.com', 'password'),
+    ('uwenayo', 'alain', 'alain@gmail.com', 'password'),
+    ('capitain', 'america','capitain@gmail.com', 'password'),
+    ('jean', 'michel', 'jeanmichel@gmail.com', 'password');
+
+-- TIP 7:
+
+SELECT * FROM USERS WHERE id = 1;
+
+SELECT first_name, last_name FROM users WHERE id = 1;
+
+-- TIP 8:
+
+-- SELECT TOP (4) * FROM USERS WHERE password = 'password'; # not supported by mysql
+SELECT * from Users  WHERE password = 'password' LIMIT 1;
