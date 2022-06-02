35c9d65407676ecc7e874c840a0d9426ae0293e5
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-06-02 15:39:30 +0200

    done cat

--- sql/recursiveQueries.sql
+++ sql/recursiveQueries.sql
@@ -1,27 +1,20 @@
-CREATE TABLE employees (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    manager_id INT,
-    INDEX(manager_id),
-    FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
-
-INSERT INTO employees (id, name, manager_id)
-VALUES (1, "Yasmina",NULL),
-(2,"John",1),
-(3,"Tarek",1),
-(4,"Pedro",2),
-(5,"Sarah",4),
-(6,"Pierre",4),
-(8,"Adil",3),
-(9,"Sara",3);
-
-WITH RECURSIVE EMPLOYEES_EXTENDED(id,name,path)
+DELIMITER #
+CREATE PROCEDURE UWENAYOAlainPacifiqueSubordinates(
+    IN input_manager_id INT
+    )
+BEGIN
+	WITH RECURSIVE subordinates_query(employee_id,manager_id,full_name)
     AS
-    (SELECT id,name,CAST(id as char(200))
-    FROM employees
-    WHERE manager_id IS NULL
-    UNION ALL
-    SELECT s.id, s.name, CONCAT(M.path , ",", s.id)
-    FROM EMPLOYEES_EXTENDED M JOIN employees s ON M.id=s.manager_id)
-    SELECT * FROM EMPLOYEES_EXTENDED ORDER BY path;
+    (
+        SELECT employee_id,manager_id,full_name
+        FROM employees
+        WHERE employee_id = input_manager_id
+        UNION ALL
+        SELECT emp.employee_id, emp.manager_id, emp.full_name
+        FROM employees emp JOIN subordinates_query sub ON emp.manager_id=sub.employee_id
+    )
+    SELECT * FROM subordinates_query;
+END#
+DELIMITER;
+
+CALL UWENAYOALainPacifiqueSubordinates(2);
