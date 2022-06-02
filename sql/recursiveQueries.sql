a3c41a65b9b12146e698194455456c082ccd142e
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-06-02 13:04:20 +0200

    recursive queries

--- sql/recursiveQueries.sql
+++ sql/recursiveQueries.sql
@@ -1,13 +1,27 @@
-CREATE TABLE IF NOT EXISTS `sample` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    manager_id INT,
+    INDEX(manager_id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
 
-INSERT INTO `sample` (`name`, `email`, `password`) VALUES
-('John Doe', 'joe@gmail.com', 'password'),
-('Jane Doe', 'jane@gmail.com', 'password')
-('Tester King', 'tester@gmail.com', 'password');
+INSERT INTO employees (id, name, manager_id)
+VALUES (1, "Yasmina",NULL),
+(2,"John",1),
+(3,"Tarek",1),
+(4,"Pedro",2),
+(5,"Sarah",4),
+(6,"Pierre",4),
+(8,"Adil",3),
+(9,"Sara",3);
+
+WITH RECURSIVE EMPLOYEES_EXTENDED(id,name,path)
+    AS
+    (SELECT id,name,CAST(id as char(200))
+    FROM employees
+    WHERE manager_id IS NULL
+    UNION ALL
+    SELECT s.id, s.name, CONCAT(M.path , ",", s.id)
+    FROM EMPLOYEES_EXTENDED M JOIN employees s ON M.id=s.manager_id)
+    SELECT * FROM EMPLOYEES_EXTENDED ORDER BY path;
